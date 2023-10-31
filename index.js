0c7b7e7b5b29a4d3994cff158d67c78eef291ca9
Rafaela Simões <rafaelasimoes2203@gmail.com>
2023-10-31 14:54:10 -0300

    acessando tabela do banco

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.use('/', router);
 const ipAddress = '172.16.31.36'; //Endereço IP da máquina
 const port = 3003;
 
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost', 
+  user: 'root', 
+  password: 'acesso123', 
+  database: 'site_compras',
+});
+
+
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Erro ao conectar ao banco de dados:', err);
+    } else {
+      console.log('Conectado ao banco de dados MySQL');
+    }
+  });
+
+
 app.listen(port, ipAddress, () => {
     console.log(`Servidor rodando em http://${ipAddress}:${port}`);
 });
