1d309efd4ac2083979a42b4000085f8c5ba0c690
Rafaela Simões <rafaelasimoes2203@gmail.com>
2023-11-06 17:20:25 -0300

    inserir e buscar as informações do banco

--- index.js
+++ index.js
@@ -2,35 +2,71 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const bodyParser = require('body-parser'); // Para processar os dados do formulário
 
-router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Rota para processar o login
+router.post('/login', function(req, res) {
+    const { username, password } = req.body;
+
+    // Consultar o banco de dados para verificar as credenciais
+    const query = 'SELECT * FROM users WHERE nome = ? AND senha = ?';
+    connection.query(query, [username, password], (err, results) => {
+        if (err) {
+            console.error('Erro ao consultar o banco de dados:', err);
+            res.status(500).send('Erro interno.');
+        } else {
+            if (results.length > 0) {
+                res.send('Login bem-sucedido!');
+            } else {
+                res.send('Credenciais inválidas. Por favor, tente novamente.');
+            }
+        }
+    });
+});
+
+// Rota para processar o cadastro
+router.post('/signup', function(req, res) {
+    const { newUsername, newPassword } = req.body;
+
+    // Inserir novas informações na tabela users
+    const query = 'INSERT INTO users (nome, senha) VALUES (?, ?)';
+    connection.query(query, [newUsername, newPassword], (err, results) => {
+        if (err) {
+            console.error('Erro ao cadastrar usuário:', err);
+            res.status(500).send('Erro interno.');
+        } else {
+            res.send('Cadastro bem-sucedido! Você pode fazer o login agora.');
+        }
+    });
+});
 
 app.use('/', router);
 
-const ipAddress = '172.16.31.36'; //Endereço IP da máquina
+const ipAddress = '172.16.31.36'; // Endereço IP da máquina
 const port = 3003;
 
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost', 
-  user: 'root', 
-  password: 'acesso123', 
-  database: 'site_compras',
+    host: 'localhost',
+    user: 'root',
+    password: 'acesso123',
+    database: 'site_compras',
 });
-
-
 
 connection.connect((err) => {
     if (err) {
-      console.error('Erro ao conectar ao banco de dados:', err);
+        console.error('Erro ao conectar ao banco de dados:', err);
     } else {
-      console.log('Conectado ao banco de dados MySQL');
+        console.log('Conectado ao banco de dados MySQL');
     }
-  });
-
+});
 
 app.listen(port, ipAddress, () => {
     console.log(`Servidor rodando em http://${ipAddress}:${port}`);
